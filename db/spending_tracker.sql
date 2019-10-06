e82737038ec106f3c39f1043ec793011f110e749
Ross Cobban <rosscobban@me.com>
2019-10-06 11:07:18 +0100

    Initial Commit

---
+++ db/spending_tracker.sql
@@ -0,0 +1,20 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE tags;
+
+CREATE TABLE merchants(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags(
+  id SERIAL8 PRIMARY KEY,
+  type VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL8 PRIMARY KEY,
+  amount INT4,
+  merchant_id INT8 REFERENCES merchants(id),
+  tag_id INT8 REFERENCES tags(id)
+);
