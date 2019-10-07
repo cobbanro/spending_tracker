8c99d8c284b4000a43614179bccd169309a60057
Ross Cobban <rosscobban@me.com>
2019-10-07 09:11:09 +0100

    Fixed tag attribute

--- controller.rb
+++ controller.rb
@@ -6,6 +6,8 @@ require_relative('./models/tag')
 require_relative('./models/transaction')
 also_reload('./models/*')
 
+@merchant_list = []
+
 get '/home' do
   @merchant = Merchant.all()
   erb(:home)
@@ -13,4 +15,9 @@ end
 
 get '/home/new-retailer' do
   erb(:newretailer)
+end
+
+post '/home' do
+  @merchant_list.push(params)
+  redirect to('/home')
 end
